1a954f869702d8aaccbf5d6c727713b1890dc9fb
Bryan Alcantara <bryanalcantara@Bryans-MacBook-Air.local>
2024-09-30 21:20:13 -0400

    reconfig db file

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,12 +1,19 @@
 const pgp = require("pg-promise")();
 require("dotenv").config();
 
-const cn = {
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE,
-  user: process.env.PG_USER,
-};
+const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER, PG_PASSWORD } =
+  process.env;
+const cn = DATABASE_URL
+  ? {
+      connectionString: DATABASE_URL,
+      max: 30,
+    }
+  : {
+      host: PG_HOST,
+      port: PG_PORT,
+      database: PG_DATABASE,
+      user: PG_USER,
+    };
 
 const db = pgp(cn);
 
@@ -14,7 +21,9 @@ db.connect()
   .then((cn) => {
     const { user, host, port, database } = cn.client;
     console.log(
-      `Postgres connection established with user:\x1b[33m${user}\x1b[0m, host:\x1b[33m${host}\x1b[0m,  port:\x1b[33m${port}\x1b[0m, database:\x1b[33m${database}\x1b[0m`
+      "\x1b[90m" +
+        `Postgres connection established with user:${user}, host:${host}, port:${port}, database:${database}` +
+        "\x1b[0m"
     );
     cn.done();
   })
